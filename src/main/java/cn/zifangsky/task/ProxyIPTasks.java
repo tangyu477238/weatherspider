223c24f78e22fa21a49870ff64ce0aad86cbf843
25306 <253060436@qq.com>
2021-08-26 07:04:40 +0800

    增加线程池处理

--- src/main/java/cn/zifangsky/task/ProxyIPTasks.java
+++ src/main/java/cn/zifangsky/task/ProxyIPTasks.java
@@ -2,8 +2,12 @@ package cn.zifangsky.task;
 
 import cn.zifangsky.common.ExecutorProcessPool;
 import cn.zifangsky.manager.CrawlManager;
+import cn.zifangsky.manager.ProxyIpManager;
 import cn.zifangsky.model.Gupiao;
+import cn.zifangsky.model.ProxyIp;
+import cn.zifangsky.model.bo.ProxyIpBO;
 import cn.zifangsky.mq.consumer.GupiaoCodeKlineReceiver;
+import cn.zifangsky.mq.producer.CheckIPSender;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,6 +18,7 @@ import java.text.Format;
 import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 /**
  * 定时任务配置
@@ -34,6 +39,11 @@ public class ProxyIPTasks {
     @Value("${mq.proxyIp.off}")
     private String proxyOff;
 
+    @Resource
+    private ProxyIpManager proxyIpManager;
+
+    @Resource
+    private CheckIPSender checkIPSender;
 
     /**
      * 代理IP定时获取任务1
@@ -85,5 +95,36 @@ public class ProxyIPTasks {
 
 
 
+    /**
+     * 代理IP定时检测任务（检查是否有效）
+     * @author zifangsky
+     * @date 2018/6/21 13:50
+     * @since 1.0.0
+     */
+    @Scheduled(cron = "${task.checkProxyIp.schedule}")
+    public void checkProxyIpTask(){
+        Date current = new Date();
+        log.debug(MessageFormat.format("开始执行代理IP定时检测任务，Date：{0}",FORMAT.format(current)));
 
+        //1 查询数据库中所有代理IP
+        List<ProxyIp> list = proxyIpManager.selectAll();
+
+        if(list != null && list.size() > 0){
+            //2 遍历
+            list.forEach(proxyIp -> {
+                ProxyIpBO proxyIpBO = new ProxyIpBO();
+                proxyIpBO.setId(proxyIp.getId());
+                proxyIpBO.setIp(proxyIp.getIp());
+                proxyIpBO.setPort(proxyIp.getPort());
+                proxyIpBO.setType(proxyIp.getType());
+                proxyIpBO.setAddr(proxyIp.getAddr());
+                proxyIpBO.setUsed(proxyIp.getUsed());
+                proxyIpBO.setOther(proxyIp.getOther());
+                proxyIpBO.setCheckType(ProxyIpBO.CheckIPType.UPDATE);
+
+                //3 添加到队列中
+                checkIPSender.send(proxyIpBO);
+            });
+        }
+    }
 }
