882a04055ddf47872f1956a143eaff13a3de7f97
25306 <253060436@qq.com>
2021-08-28 21:18:32 +0800

    验证ip

--- src/main/java/cn/zifangsky/common/CheckIPUtils.java
+++ src/main/java/cn/zifangsky/common/CheckIPUtils.java
@@ -32,17 +32,17 @@ public class CheckIPUtils {
 			connection.setReadTimeout(4000);
 			connection.setConnectTimeout(4000);
 			connection.setRequestMethod("GET");
-			String msg = "";
+			StringBuffer msg = new StringBuffer();
 			if(connection.getResponseCode() == 200){
 				BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 				String line;
 				while ((line = reader.readLine()) != null) { // 循环从流中读取
-					msg += line + "\n";
+					msg.append(line).append("\n");
 				}
 				reader.close(); // 关闭流
 			}
 
-			if (msg.startsWith("jQuery")){
+			if (msg.toString().startsWith("jQuery")){
 				log.info(MessageFormat.format("============代理IP[{0} {1}]可用=====================", ip,port));
 				return true;
 			}
@@ -91,11 +91,11 @@ public class CheckIPUtils {
 //					return  true;
 //				}
 //			}
-			String url = "http://"+System.currentTimeMillis()
-					+".push2his.eastmoney.com/api/qt/stock/kline/get?cb=jQuery112403780605306048155_1618930055627" +
-					"&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5%2Cf6&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58%2Cf59%2Cf60%2Cf61" +
-					"&ut=7eea3edcaed734bea9cbfc24409ed989&klt=30&fqt=1&secid=0.000001&beg=20210827&end=20210827&_=1618930055730";
-			return checkValidIP(ip, port, new URL(url));
+			StringBuffer url = new StringBuffer("http://");
+					url.append(System.currentTimeMillis());
+					url.append(".push2his.eastmoney.com/api/qt/stock/kline/get?cb=jQuery112403780605306048155_1618930055627");
+					url.append("&fields1=f1%2Cf2%2Cf3%2Cf4%2Cf5%2Cf6&fields2=f51%2Cf52%2Cf53%2Cf54%2Cf55%2Cf56%2Cf57%2Cf58%2Cf59%2Cf60%2Cf61&ut=7eea3edcaed734bea9cbfc24409ed989&klt=30&fqt=1&secid=0.000001&beg=20210827&end=20210827&_=1618930055730");
+			return checkValidIP(ip, port, new URL(url.toString()));
 		} catch (Exception e) {
 			log.debug(e.toString());
 		}
