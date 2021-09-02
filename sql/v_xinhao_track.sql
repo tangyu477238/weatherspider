be3feefb27d72a78b437bff10789ec25b996a6db
tangzhiyu <tangzhiyu@vldtech.com>
2021-09-02 09:50:19 +0800

    增加日志信息

--- sql/v_xinhao_track.sql
+++ sql/v_xinhao_track.sql
@@ -5,16 +5,16 @@ drop view v_xinhao_track;
 create view v_xinhao_track as
 
 select
-s.symbol,30 as period,s.biz_date,s.price1 as loss_price, 1 as stype
+    s.symbol,30 as period,s.biz_date,s.price1 as loss_price, 1 as stype
 from gupiao_xinhao s
-inner join v_syn_max_bizdate v on s.biz_date > v.biz_date
 where  (s.sj3<s.sj2 and s.sj2<s.sj1)  and s.type = 1 and s.period = 30
+  and s.biz_date >= DATE_FORMAT(ADDDATE(NOW(),-1),'%Y-%m-%d')
 
 union all
 
 select
-s.symbol,30 as period,s.biz_date,s.price1 as loss_price, 1 as stype
+    s.symbol,30 as period,s.biz_date,s.price1 as loss_price, 1 as stype
 from gupiao_xinhao s
-inner join v_syn_max_bizdate v on s.biz_date > v.biz_date
 where  (s.sj5<s.sj4 and s.sj4<s.sj3 and s.sj3>s.sj2 and s.sj2<s.sj1)  and s.type = 1 and s.period = 30
+  and s.biz_date >= DATE_FORMAT(ADDDATE(NOW(),-1),'%Y-%m-%d')
 ;
