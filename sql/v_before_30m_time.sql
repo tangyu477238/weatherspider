216ab8c2bb18bcb74b853cee9597bb5f2c38140d
25306 <253060436@qq.com>
2021-09-06 21:52:22 +0800

    代理ip设置

--- sql/v_before_30m_time.sql
+++ sql/v_before_30m_time.sql
@@ -6,4 +6,6 @@ create view v_before_30m_time as
 select case
 when DATE_FORMAT(NOW(),'%H')=12 then CONCAT(DATE_FORMAT(NOW(),'%Y-%m-%d'),' 11:30')
 when DATE_FORMAT(NOW(),'%H')=13 and DATE_FORMAT(NOW(),'%i')<30 then CONCAT(DATE_FORMAT(NOW(),'%Y-%m-%d'),' 11:30')
+when DATE_FORMAT(NOW(),'%H')>14 then CONCAT(DATE_FORMAT(NOW(),'%Y-%m-%d'),' 15:00')
+when DATE_FORMAT(NOW(),'%H')<10 then CONCAT(DATE_FORMAT(ADDDATE(NOW(),-1),'%Y-%m-%d'),' 15:00')
 when DATE_FORMAT(NOW(),'%i')<30 then DATE_FORMAT(NOW(),'%Y-%m-%d %H:00') else DATE_FORMAT(NOW(),'%Y-%m-%d %H:30') end as biz_date
