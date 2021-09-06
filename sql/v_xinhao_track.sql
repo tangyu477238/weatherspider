9dc044ad705e82e0e1cc0700d415a69c94673343
tangzhiyu <tangzhiyu@vldtech.com>
2021-09-06 14:29:49 +0800

    增加日志信息

--- sql/v_xinhao_track.sql
+++ sql/v_xinhao_track.sql
@@ -4,6 +4,7 @@ drop view v_xinhao_track;
 
 create view v_xinhao_track as
 
+-- 小小大
 select
     s.symbol,30 as period,s.biz_date,s.price1 as loss_price, 1 as stype
 from gupiao_xinhao s
@@ -12,17 +13,19 @@ where  (s.sj3<s.sj2 and s.sj2<s.sj1)  and s.type = 1 and s.period = 30
 
 union all
 
+-- 大小大
 select
-    s.symbol,30 as period,s.biz_date,s.price1 as loss_price, 1 as stype
+    s.symbol,30 as period,if(s.price3<s.price1,s.biz_date3,s.biz_date) as biz_date,
+       if(s.price3<s.price1,s.price3,s.price1) as loss_price, 1 as stype
 from gupiao_xinhao s
-where  (s.sj5<s.sj4 and s.sj4<s.sj3 and s.sj3>s.sj2 and s.sj2<s.sj1)  and s.type = 1 and s.period = 30
+where  (s.sj3>s.sj2 and s.sj2<s.sj1)  and s.type = 1 and s.period = 30
   and s.biz_date >= DATE_FORMAT(ADDDATE(NOW(),-1),'%Y-%m-%d')
 
--- 暂时未生效
+--  小大小
 union all
 select
-    s.symbol,30 as period,s.biz_date,if(s.price4<s.price2,s.price2,s.price4) as loss_price, 1 as stype
+    s.symbol,30 as period,s.biz_date2,s.price2 as loss_price, 1 as stype
 from gupiao_xinhao s
-where  (s.sj4>s.sj3 and s.sj3<s.sj2 and s.sj2>s.sj1)  and s.type = 1 and s.period = 30
+where  (s.sj3<s.sj2 and s.sj2>s.sj1)  and s.type = 1 and s.period = 30
   and s.biz_date >= DATE_FORMAT(ADDDATE(NOW(),-1),'%Y-%m-%d')
 ;
