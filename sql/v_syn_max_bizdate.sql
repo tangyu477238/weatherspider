205cd0a079e2fae25ec544d7fa629bd5d3311131
25306 <253060436@qq.com>
2021-10-08 09:41:29 +0800

    代理ip设置

--- sql/v_syn_max_bizdate.sql
+++ sql/v_syn_max_bizdate.sql
@@ -1,12 +1,15 @@
 -- 查询同步的最大日期
-create view v_syn_max_bizdate   as 
+
+drop view v_syn_max_bizdate;
+create view v_syn_max_bizdate   as
 
 select max(biz_date) as  biz_date from (
 
 select MAX(c.biz_date) as biz_date 
 from biz_calendar  c
 inner join (
-SELECT holiday from biz_calendar where biz_date = date_format(now(),'%Y-%m-%d')
+SELECT holiday from biz_calendar where biz_date = IF(date_format(now(),'%Y-%m-%d %H:%i:%s') < date_format(now(),'%Y-%m-%d %09:%25:%00'),
+date_format(ADDDATE(NOW(), -1),'%Y-%m-%d'), date_format(now(),'%Y-%m-%d'))
 ) x on x.holiday = 1 
 where c.holiday = 1 and c.biz_date = date_format(now(),'%Y-%m-%d') and date_format(now(),'%Y-%m-%d %H:%i:%s') > date_format(now(),'%Y-%m-%d %09:%25:%00') 
 
@@ -14,7 +17,8 @@ union all
 
 select MAX(c.biz_date) as biz_date from biz_calendar  c
 inner join (
-SELECT holiday from biz_calendar where biz_date = date_format(now(),'%Y-%m-%d')
+SELECT holiday from biz_calendar where biz_date = IF(date_format(now(),'%Y-%m-%d %H:%i:%s') < date_format(now(),'%Y-%m-%d %09:%25:%00'),
+date_format(ADDDATE(NOW(), -1),'%Y-%m-%d'), date_format(now(),'%Y-%m-%d'))
 ) x on x.holiday = 1 
 where c.holiday = 1 and c.biz_date < date_format(now(),'%Y-%m-%d') and date_format(now(),'%Y-%m-%d %H:%i:%s') < date_format(now(),'%Y-%m-%d %09:%25:%00')
 
@@ -22,7 +26,8 @@ union all
 
 select MAX(biz_date) as biz_date from biz_calendar c
 inner join (
-SELECT holiday from biz_calendar where biz_date = date_format(now(),'%Y-%m-%d')
+SELECT holiday from biz_calendar where biz_date = IF(date_format(now(),'%Y-%m-%d %H:%i:%s') < date_format(now(),'%Y-%m-%d %09:%25:%00'),
+date_format(ADDDATE(NOW(), -1),'%Y-%m-%d'), date_format(now(),'%Y-%m-%d'))
 ) x on x.holiday = 0 
 where biz_date < date_format(now(),'%Y-%m-%d') and c.holiday = 1
 
