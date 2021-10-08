cb22a35b7c253d0d62fc4c1d885fa281cc381971
25306 <253060436@qq.com>
2021-10-08 10:37:00 +0800

    代理ip设置

--- sql/v_xinhao_track.sql
+++ sql/v_xinhao_track.sql
@@ -8,8 +8,9 @@ create view v_xinhao_track as
 select
     s.symbol,30 as period,s.biz_date,s.price1 as loss_price, 1 as stype
 from gupiao_xinhao s
+inner join v_syn_max_bizdate b on  s.biz_date >= date_add(b.biz_date, interval -1 day)
 where  (s.sj3<s.sj2 and s.sj2<s.sj1)  and s.type = 1 and s.period = 30
-  and s.biz_date >= DATE_FORMAT(ADDDATE(NOW(),-1),'%Y-%m-%d')
+
 
 union all
 
@@ -18,7 +19,8 @@ select
     s.symbol,30 as period,if(s.price3<s.price1,s.biz_date3,s.biz_date) as biz_date,
        if(s.price3<s.price1,s.price3,s.price1) as loss_price, 1 as stype
 from gupiao_xinhao s
+inner join v_syn_max_bizdate b on  s.biz_date >= date_add(b.biz_date, interval -1 day)
 where  (s.sj3>s.sj2 and s.sj2<s.sj1)  and s.type = 1 and s.period = 30
-  and s.biz_date >= DATE_FORMAT(ADDDATE(NOW(),-1),'%Y-%m-%d')
+
 
 ;
