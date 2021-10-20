b85310f80230fccb40aa9d69015554e824bc0ff9
25306 <253060436@qq.com>
2021-10-20 21:49:07 +0800

    代理ip设置

--- sql/v_buylist_5m-03.sql
+++ sql/v_buylist_5m-03.sql
@@ -4,54 +4,40 @@ drop view v_buylist_5m;
 create view  v_buylist_5m as
 
 
-select DISTINCT x3.symbol,x3.biz_date,x3.price3  as lossPrice,g.name
-from gupiao_xinhao x1
--- inner join v_before_120m_time v on x1.biz_date >= v.biz_date
-inner join gupiao_xinhao x3 on x3.period = 5 and x3.symbol = x1.symbol
-inner join gupiao g on g.symbol = x1.symbol
-	and ( x3.sj5 >0 and x3.sj4 < x3.sj3 and x3.sj3 > x3.sj2 and x3.sj2 > x3.sj1)
-inner join gupiao_xinhao x2 on x2.period = 30 and x2.symbol = x1.symbol and x2.sj1 = 0
-where x1.period = 15 and ( x1.sj3 < x1.sj2 and x1.sj2 < x1.sj1)
+select
+    x3.symbol, x3.biz_date, x3.price3  as lossPrice,g.name,x3.period
+from gupiao_xinhao x3
+         inner join gupiao g on g.symbol = x3.symbol and x3.period = 15 and x3.type_name = 'zjrc' and ( x3.sj5 >0 and x3.sj4 < x3.sj3 and x3.sj3 > x3.sj2 and x3.sj2 > x3.sj1)
+         inner join (
+    select
+        x1.symbol,x1.biz_date
+    from gupiao_xinhao x1
+             inner join v_syn_max_bizdate v on x1.biz_date >= v.biz_date
+    where x1.period = 30 and x1.type_name = 'zjrc' and ( x1.sj3 < x1.sj2 and x1.sj2 < x1.sj1)
+) x1 on x1.symbol = x3.symbol and x3.biz_date >=x1.biz_date
+inner join (
+    select x1.symbol
+    from gupiao_xinhao x1
+             inner join v_before_60m_time v on x1.biz_date = v.biz_date
+    where  x1.period = 60 and  x1.type_name = 'zjrc' and x1.sj1 = 0
+) x2 on  x3.symbol = x2.symbol
 
-and x1.biz_date >='2021-10-14 15:00'
-and x2.biz_date ='2021-10-14 15:00'
-and x3.biz_date >'2021-10-14 15:00'
+union all
 
-order by biz_date desc
+select
+    x3.symbol,x3.biz_date,x3.price3  as lossPrice,g.name,x3.period
+from gupiao_xinhao x3
+         inner join gupiao g on g.symbol = x3.symbol and x3.period = 30 and x3.type_name = 'zjrc' and ( x3.sj5 >0 and x3.sj4 < x3.sj3 and x3.sj3 > x3.sj2 and x3.sj2 > x3.sj1)
+         inner join (
+    select
+        x1.symbol,x1.biz_date
+    from gupiao_xinhao x1
+inner join (
+    select  MIN(biz_date) as biz_date  from (
+        select c.biz_date from v_syn_max_bizdate b
+        inner join biz_calendar c on c.biz_date <= b.biz_date and c.holiday = 1
+        order by biz_date desc  LIMIT 0, 5 ) t
+) b on  x1.biz_date >= b.biz_date
 
-
-
-
-select DISTINCT x3.symbol,x3.biz_date,x3.price3  as lossPrice,g.name
-from gupiao_xinhao x1
-inner join gupiao g on g.symbol = x1.symbol
--- inner join v_before_120m_time v on x1.biz_date >= v.biz_date
-inner join gupiao_xinhao x3 on x3.period = 15 and x3.symbol = x1.symbol
-	and ( x3.sj5 >0 and x3.sj4 < x3.sj3 and x3.sj3 > x3.sj2 and x3.sj2 > x3.sj1)
-inner join gupiao_xinhao x2 on x2.period = 60 and x2.symbol = x1.symbol and x2.sj1 = 0
-where x1.period = 30 and ( x1.sj3 < x1.sj2 and x1.sj2 < x1.sj1)
-
-and x1.biz_date >='2021-10-14 15:00'
-and x2.biz_date ='2021-10-14 15:00'
-and x3.biz_date >'2021-10-14 15:00'
-
-order by biz_date desc
-
-
-
-select DISTINCT x3.symbol,x3.biz_date,x3.price3  as lossPrice,g.name
-from gupiao_xinhao x1
-inner join gupiao g on g.symbol = x1.symbol
--- inner join v_before_120m_time v on x1.biz_date >= v.biz_date
-inner join gupiao_xinhao x3 on x3.period = 30 and x3.symbol = x1.symbol
-	and ( x3.sj5 >0 and x3.sj4 < x3.sj3 and x3.sj3 > x3.sj2 and x3.sj2 > x3.sj1)
-inner join gupiao_xinhao x2 on x2.period = 120 and x2.symbol = x1.symbol and x2.sj1 = 0
-where x1.period = 60 and ( x1.sj3 < x1.sj2 and x1.sj2 < x1.sj1)
-
-and x1.biz_date >='2021-10-12 15:00'
-and x2.biz_date ='2021-10-12 15:00'
-and x3.biz_date >'2021-10-12 15:00'
-
-order by biz_date desc
-
-
+    where x1.period = 60 and x1.type_name = 'zjrc' and ( x1.sj3 < x1.sj2 and x1.sj2 < x1.sj1)
+) x1 on x1.symbol = x3.symbol and x3.biz_date >=x1.biz_date
