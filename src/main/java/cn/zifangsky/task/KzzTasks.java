6c01b35982cd285d2969fb61de9eb645132564e2
tangzhiyu <tangzhiyu@vldtech.com>
2021-11-25 11:09:00 +0800

    lastMorning

--- src/main/java/cn/zifangsky/task/KzzTasks.java
+++ src/main/java/cn/zifangsky/task/KzzTasks.java
@@ -1,32 +1,23 @@
 package cn.zifangsky.task;
 
-import cn.hutool.json.JSONArray;
-import cn.hutool.json.JSONObject;
-import cn.hutool.json.JSONUtil;
-import cn.zifangsky.common.ComUtil;
 import cn.zifangsky.common.DateTimeUtil;
 import cn.zifangsky.login.LoginManager;
-import cn.zifangsky.manager.DongfangManager;
 import cn.zifangsky.manager.GupiaoCanUseManager;
-import cn.zifangsky.manager.GupiaoManager;
 import cn.zifangsky.manager.LastMornManager;
-import cn.zifangsky.model.BaseGupiaoKline;
-import cn.zifangsky.model.Gupiao;
 import cn.zifangsky.repository.GupiaoCanUseRepository;
 import cn.zifangsky.repository.GupiaoKlineRepository;
+import com.google.common.primitives.Ints;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.math.BigDecimal;
 import java.text.Format;
 import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 /**
  * 定时任务配置
@@ -104,7 +95,7 @@ public class KzzTasks {
     }
 
     /**
-     * //MA数据 14点50进行
+     * //MA数据 14点57进行
      * @throws Exception
      */
     @Scheduled(cron = "${task.kzz.risedown}")
@@ -112,6 +103,12 @@ public class KzzTasks {
         if ("0".equals(consumerOff)) return;
         Date current = new Date();
         log.info(MessageFormat.format("listMa，Date：{0}",FORMAT.format(current)));
+        int[] hours = {14};
+        int[] minutes = {57, 58, 59};
+        if (!checkRunTime(hours, minutes)){
+            return;
+        }
+        log.info(MessageFormat.format("进行处理，Date：{0}",FORMAT.format(current)));
         lastMornManager.listMa();
 
     }
@@ -125,10 +122,32 @@ public class KzzTasks {
         if ("0".equals(consumerOff)) return;
         Date current = new Date();
         log.info(MessageFormat.format("listMa，Date：{0}",FORMAT.format(current)));
+        int[] hours = {9};
+        int[] minutes = {35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45};
+        if (!checkRunTime(hours, minutes)){
+            return;
+        }
+        log.info(MessageFormat.format("进行处理，Date：{0}",FORMAT.format(current)));
         lastMornManager.listMa();
 
     }
 
-
+    /**
+     *  判断时间区间
+     * @param hours
+     * @param minutes
+     * @return
+     */
+    private boolean checkRunTime(int[] hours, int[] minutes){
+        List<Integer> hourList = Ints.asList(hours);
+        int cHours = DateTimeUtil.getHoursOfDay(new Date());
+        List<Integer> minuteList = Ints.asList(minutes);
+        int cMinute = DateTimeUtil.getMinutesOfHour(new Date());
+        //判断是否在范围内
+        if (hourList.contains(cHours) && minuteList.contains(cMinute)){
+            return true;
+        }
+        return false;
+    }
 
 }
